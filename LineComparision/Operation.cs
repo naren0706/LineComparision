8415faeb57f3d799d9229318d767870eb51a6466
Narenthra Kishore RS <narenthrakishore.rs@tvsnext.com>
2023-07-12 11:04:44 +0530

    [Narenthra kishore] Add : new Feature Line Length

---
+++ LineComparision/Operation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparision
+{
+    class Operation
+    {
+        int x1, y1, x2, y2;
+        public Operation(int x1, int y1, int x2, int y2) { 
+            this.x1 = x1;
+            this.y1 = y1;   
+            this.x2 = x2;   
+            this.y2 = y2;
+        }
+        public void CalculateLength()
+        {
+            double length = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2) );
+            Console.WriteLine("The length of the line is"+length);
+        }
+    }
+}
--- LineComparision/Program.cs
+++ LineComparision/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace LineComparision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operation operation = new Operation(1,2,3,4);
+            operation.CalculateLength();
+        }
+    }
+}
